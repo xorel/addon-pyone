b138bf2f26e178a9415910f72c51ab455ec564a4
Rafael <rafael@celpax.com>
2018-03-04 17:27:09 +0100

    minor fix

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6'
     ],
 
